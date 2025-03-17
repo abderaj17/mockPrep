badef95e361b7873a462ffee5fd36f6020b40536
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-17 14:27:14 +0530

    com

--- ques.js
+++ ques.js
@@ -1,13 +1,35 @@
 //questions ---
 
-function reversedStr(str){
-    let reverse = "";
+// q-1 To revese a string
 
-    for(let i = str.length-1; i >= 0; i--){
-        reverse += str[i];
-    }
-return reverse;
+function reversedStr(str){
+//     let reverse = "";
+
+//     for(let i = str.length-1; i >= 0; i--){
+//         reverse += str[i];
+//     }
+// return reverse;
+
+return str.split("").reverse().join("");
     
 }
 
 reversedStr("abderaj");
+
+
+// q-2 Even Odd check---
+
+function evenOdd(num){
+    if(num % 2 == 0){
+        return "Even"
+    }
+    return "Odd";
+}
+console.log(evenOdd(8));
+
+
+//q-3 Factorial
+
+function factorialCal(num){
+    let fact = num.reduce((acc, n)=> )
+}

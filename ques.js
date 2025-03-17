6706640497b8c00453adbb0529d25341abac6cbf
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-17 14:16:24 +0530

    com

---
+++ ques.js
@@ -0,0 +1,13 @@
+//questions ---
+
+function reversedStr(str){
+    let reverse = "";
+
+    for(let i = str.length-1; i >= 0; i--){
+        reverse += str[i];
+    }
+    console.log(reverse)
+    
+}
+
+reversedStr("abderaj");
